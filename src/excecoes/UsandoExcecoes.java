b13bde92f4c2727f6e761108bf7b432e9be3b4d4
Glaydson <glaydsonvs@gmail.com>
2019-08-08 22:01:23 -0300

    Corrigindo a saída do programa

--- src/excecoes/UsandoExcecoes.java
+++ src/excecoes/UsandoExcecoes.java
@@ -6,6 +6,7 @@ public class UsandoExcecoes {
 		try {
 			lancaExcecao();
 		} catch (Exception e) { // exceção lançada por lancaExcecao
+			System.out.flush();
 			System.err.println("Exceção tratada no método main");
 		}
 		naoLancaExcecao();
@@ -16,11 +17,13 @@ public class UsandoExcecoes {
 			System.out.println("Método lancaExcecao");
 			throw new Exception(); // gera exceção
 		} catch (Exception e) { // captura a exceção lançada no try
+			System.out.flush();
 			System.err.println("Exceção tratada no método lancaExcecao");
 			throw e; // relança para processamento posterior
 
 			// qualquer código aqui nunca será executado, irá gerar erro de compilação
 		} finally { // executa independente do que ocorra no try...catch
+			System.out.flush();
 			System.err.println("Finally executado em lancaExcecao");
 		}
 	}
@@ -29,8 +32,10 @@ public class UsandoExcecoes {
 		try {   // o bloco try não lança uma exceção
 			System.out.println("Método naoLancaExcecao");
 		} catch (Exception e) {    // não executa
+			System.out.flush();
 			System.err.println(e);
 		} finally {   // executa independente do que ocorra no try...catch
+			System.out.flush();
 			System.err.println("Finally executado em naoLancaExcecao");
 		}
 		System.out.println("Fim do método naoLancaExcecao");
